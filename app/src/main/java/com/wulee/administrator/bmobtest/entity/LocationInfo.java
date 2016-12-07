83ae3fd6fb820287790ac4d0662ce6525654934d
wulee <wulee@e00737d9-b068-4628-95e4-48cd3335de4d>
2016-12-07 15:32:53 +0000

    bmob

---
+++ app/src/main/java/com/wulee/administrator/bmobtest/entity/LocationInfo.java
@@ -0,0 +1,15 @@
+package com.wulee.administrator.bmobtest.entity;
+
+import cn.bmob.v3.BmobObject;
+
+/**
+ * Created by wulee on 2016/12/7 16:50
+ */
+
+public class LocationInfo extends BmobObject {
+    public String latitude;
+    public String lontitude;
+    public String address;
+    public String locationdescribe;
+    public String nativePhoneNumber;
+}
