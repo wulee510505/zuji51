416c31bc5a6f79fa88cf84b9256389f8a10ef619
wulee <wulee510505@gmail.com>
2017-01-14 12:40:05 +0000

    地图选点

--- app/src/main/java/com/wulee/administrator/bmobtest/utils/LocationUtil.java
+++ app/src/main/java/com/wulee/administrator/bmobtest/utils/LocationUtil.java
@@ -124,7 +124,7 @@ public class LocationUtil{
                 sb.append("\ndescribe : ");
                 sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
             }
-            speak(location.getAddrStr()+ location.getLocationDescribe());
+            //speak(location.getAddrStr()+ location.getLocationDescribe());
 
             LocationInfo locationInfo = new LocationInfo();
             locationInfo.latitude = location.getLatitude()+"";
