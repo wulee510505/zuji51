db35d256f4901e1c3658e5bfc730a827e211904a
wulee <wulee@e00737d9-b068-4628-95e4-48cd3335de4d>
2017-01-19 02:48:05 +0000

    添加自动更新

--- app/src/main/java/com/wulee/administrator/bmobtest/App.java
+++ app/src/main/java/com/wulee/administrator/bmobtest/App.java
@@ -26,6 +26,7 @@ public class App extends Application {
         SDKInitializer.initialize(context);
 
         initBmobSDK();
+        //BmobUpdateAgent.initAppVersion();
     }
 
     private void initBmobSDK() {
