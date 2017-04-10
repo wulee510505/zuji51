f486e78b7e5d868fe5bed48683557110f03e065e
wulee <wulee@e00737d9-b068-4628-95e4-48cd3335de4d>
2017-04-10 05:59:23 +0000

    加入反馈功能

--- app/src/main/java/com/wulee/administrator/zuji/ui/SignActivity.java
+++ app/src/main/java/com/wulee/administrator/zuji/ui/SignActivity.java
@@ -9,9 +9,13 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.test.sign_calender.DPCManager;
 import com.test.sign_calender.DPDecor;
+import com.test.sign_calender.DPMode;
+import com.test.sign_calender.DatePicker;
 import com.test.sign_calender.DatePicker2;
 import com.wulee.administrator.zuji.R;
 import com.wulee.administrator.zuji.base.BaseActivity;
@@ -28,6 +32,7 @@ import java.util.List;
 
 public class SignActivity extends BaseActivity {
 
+    private ImageView ivBack;
     private DatePicker2 mDatePicker;
 
     private List<String> dateList = new ArrayList<>();
@@ -38,24 +43,25 @@ public class SignActivity extends BaseActivity {
         setContentView(R.layout.sign_activity);
 
         initView();
-
+        addListener();
         initData();
     }
 
     private void initView() {
+        ivBack = (ImageView) findViewById(R.id.iv_back);
         mDatePicker = (DatePicker2) findViewById(R.id.datepicker);
+        mDatePicker.setMode(DPMode.SINGLE);
     }
 
     private void initData() {
-        dateList.add("2017-4-2");
-        dateList.add("2017-4-4");
-        dateList.add("2017-04-21");
-        dateList.add("2017-04-09");
+        dateList.add("2017-4-11");
+        dateList.add("2017-4-19");
         DPCManager.getInstance().setDecorBG(dateList);
 
         mDatePicker.setFestivalDisplay(true); //是否显示节日
         mDatePicker.setHolidayDisplay(true); //是否显示假期
         mDatePicker.setDeferredDisplay(true); //是否显示补休
+
 
         Calendar c = Calendar.getInstance();//首先要获取日历对象
         int mYear = c.get(Calendar.YEAR); // 获取当前年份
@@ -69,13 +75,33 @@ public class SignActivity extends BaseActivity {
                 paint.setAntiAlias(true);
                 Bitmap mBitmap = null;
                 try {
-                    try (InputStream is = getResources().openRawResource(R.raw.icon_mark)) {
+                    try (InputStream is = getResources().openRawResource(R.raw.icon_has_sign)) {
                         mBitmap = BitmapFactory.decodeStream(is);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                canvas.drawBitmap(mBitmap, rect.centerX() - mBitmap.getWidth() / 2f, rect.centerY() - mBitmap.getHeight() / 2f, paint);
+               // canvas.drawBitmap(mBitmap, rect.centerX() - mBitmap.getWidth() / 2f, rect.centerY() - mBitmap.getHeight() / 2f, paint);
+            }
+        });
+    }
+
+
+    private void addListener(){
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignActivity.this.finish();
+            }
+        });
+        mDatePicker.setOnDatePickedListener(new DatePicker.OnDatePickedListener() {
+            @Override
+            public void onDatePicked(String date) {
+               for (String signdate : dateList){
+                   if(signdate.equals(date)){
+                        toast("已经签过到了");
+                   }
+               }
             }
         });
     }
