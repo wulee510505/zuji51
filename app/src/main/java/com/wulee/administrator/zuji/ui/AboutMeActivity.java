7229b78dbe3b13658330fcf1c01d921e26f054d1
wulee <wulee@e00737d9-b068-4628-95e4-48cd3335de4d>
2017-05-27 09:42:11 +0000

    添加个人信息页

--- app/src/main/java/com/wulee/administrator/zuji/ui/AboutMeActivity.java
+++ app/src/main/java/com/wulee/administrator/zuji/ui/AboutMeActivity.java
@@ -7,12 +7,21 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.media.UMImage;
+import com.umeng.socialize.media.UMWeb;
 import com.wulee.administrator.zuji.R;
 import com.wulee.administrator.zuji.base.BaseActivity;
+import com.wulee.administrator.zuji.utils.AppUtils;
 
 
 /**
@@ -21,8 +30,8 @@ import com.wulee.administrator.zuji.base.BaseActivity;
 
 public class AboutMeActivity extends BaseActivity {
 
+    private TextView tvVersionName;
     private TextView tvSoftWareSite;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +51,10 @@ public class AboutMeActivity extends BaseActivity {
                 finish();
             }
         });
+
+        tvVersionName= (TextView)findViewById(R.id.tv_version_name);
+        String versionName = AppUtils.getVersionName();
+        tvVersionName.setText("V "+versionName);
 
         tvSoftWareSite = (TextView) findViewById(R.id.tv_software_site);
         tvSoftWareSite.setText(getClickableSpan());
@@ -65,4 +78,52 @@ public class AboutMeActivity extends BaseActivity {
         }, 5, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         return spannableString;
     }
+
+
+
+    /**
+     * 分享功能
+     */
+    public synchronized void shareClick(View view) {
+        UMImage image = new UMImage(AboutMeActivity.this, R.mipmap.ic_launcher);
+        UMWeb web = new UMWeb("http://zuji51.bmob.site/");
+        web.setTitle("足迹 \n一款可以记录并查看出行轨迹的工具类软件");//标题
+        web.setThumb(image);  //缩略图
+        web.setDescription("一款可以记录并查看出行轨迹的工具类软件");
+
+        new ShareAction(AboutMeActivity.this).withMedia(web)
+                .setDisplayList(SHARE_MEDIA.WEIXIN,SHARE_MEDIA.WEIXIN_CIRCLE,SHARE_MEDIA.WEIXIN_FAVORITE /*,SHARE_MEDIA.QQ,SHARE_MEDIA.QZONE*/)
+                .setCallback(umShareListener).open();
+    }
+
+    private UMShareListener umShareListener = new UMShareListener() {
+        @Override
+        public void onStart(SHARE_MEDIA platform) {
+            Log.d("plat","platform"+platform);
+            //分享开始的回调
+        }
+        @Override
+        public void onResult(SHARE_MEDIA platform) {
+            Log.d("plat","platform"+platform);
+            Toast.makeText(AboutMeActivity.this, platform + " 分享成功啦", Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        public void onError(SHARE_MEDIA platform, Throwable t) {
+            Toast.makeText(AboutMeActivity.this,platform + " 分享失败啦", Toast.LENGTH_SHORT).show();
+            if(t!=null){
+                Log.d("throw","throw:"+t.getMessage());
+            }
+        }
+        @Override
+        public void onCancel(SHARE_MEDIA platform) {
+            Toast.makeText(AboutMeActivity.this,platform + " 分享取消了", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
+    }
+
 }
